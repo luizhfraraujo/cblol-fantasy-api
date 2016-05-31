864bb654a07ab5f9415540e3e371d3e1bc864fa8
Luiz Henrique de F. R. Araújo <luizhfreitas@outlook.com>
2016-05-30 22:05:04 -0300

    Update production file

--- libs/config.production.js
+++ libs/config.production.js
@@ -1,7 +1,7 @@
 import logger from "./logger.js";
 
 module.exports = {
-    database: "nodeapi_test",
+    /*database: "nodeapi_test",
     username: "",
     password: "",
     params: {
@@ -14,7 +14,7 @@ module.exports = {
       define: {
         underscored: true
       }
-  },/*
+  },*/
     database: "cblol_fantasy",
     username: "tir4y",
     password: "c3v4d4",
@@ -24,7 +24,7 @@ module.exports = {
              logging: (sql) => {
                  logger.info(`[${new Date()}] ${sql}`);
              }
-  },*/
+  },
   jwtSecret: "N0d3-4p1$",
   jwtSession: {session: false}
 };
